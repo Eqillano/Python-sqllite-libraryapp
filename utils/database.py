e135e48064b6d74c0afd51c293d961be65dfddff
Sergey Vanteev <fukkrypto@gmail.com>
2020-06-01 18:03:00 +0300

    CRUD books используя SQLite и добавлен context manager

--- utils/database.py
+++ utils/database.py
@@ -2,45 +2,38 @@
 
 ###
 
-import sqlite3
+from .database_connection import DatabaseConnection
 
-
-books_file = 'books.json'
 
 
 def create_book_table():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    cursor.execute('CREATE TABLE books(name text,author text,read integer)')
-
-    connection.commit()
-    connection.close()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text,author text,read integer)')
 
 
-def _save_all_books(books):
-    with open(books_file,'w') as file:
-        json.dump(books,file)
 
 def get_all_books():
-    with open(books_file,'r') as file:
-        return json.load(file)
+    with DatabaseConnection('data.db') as connection:
+         cursor = connection.cursor()
+         cursor.execute('SELECT * FROM books')
+         books = [{'name':row[0],'author':row[1],'read':row[2]} for row in cursor.fetchall()]
+    return books
 
 def add_books(name,author):
-    books = get_all_books()
-    books.append({"name":name,'author':author,'read':False})
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+         cursor = connection.cursor()
+         cursor.execute('INSERT INTO books VALUES (?,?,0)',(name,author))
 
 
 
 def mark_book_as_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-           book['read'] = True
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+         cursor = connection.cursor()
+         cursor.execute('UPDATE books SET read = 1 WHERE name == ?',(name,))
+
 
 def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book['name'] != name]
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+         cursor = connection.cursor()
+         cursor.execute('DELETE FROM books WHERE name == ?',(name,))
