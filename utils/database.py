0d579988de2e86c3b7d6a1819a43d4cb93310f63
Sergey Vanteev <fukkrypto@gmail.com>
2020-06-01 15:48:52 +0300

    from json to sqlite3 initial commit

---
+++ utils/database.py
@@ -0,0 +1,46 @@
+###
+
+###
+
+import sqlite3
+
+
+books_file = 'books.json'
+
+
+def create_book_table():
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('CREATE TABLE books(name text,author text,read integer)')
+
+    connection.commit()
+    connection.close()
+
+
+def _save_all_books(books):
+    with open(books_file,'w') as file:
+        json.dump(books,file)
+
+def get_all_books():
+    with open(books_file,'r') as file:
+        return json.load(file)
+
+def add_books(name,author):
+    books = get_all_books()
+    books.append({"name":name,'author':author,'read':False})
+    _save_all_books(books)
+
+
+
+def mark_book_as_read(name):
+    books = get_all_books()
+    for book in books:
+        if book['name'] == name:
+           book['read'] = True
+    _save_all_books(books)
+
+def delete_book(name):
+    books = get_all_books()
+    books = [book for book in books if book['name'] != name]
+    _save_all_books(books)
